511b9266d045943ca9a882968dc44726f7a89182
bulbucus <emanuelfarinha98@gmail.com>
2020-11-19 18:46:20 +0000

    remove .env of git

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -1,11 +1,10 @@
 CREATE TABLE users (
-    id_users BIGSERIAL NOT NULL PRIMARY KEY,
+    id_users UUID NOT NULL DEFAULT uuid_generate_v4(),
     primeiro_nome VARCHAR(30) NOT NULL,
     ultimo_nome VARCHAR(30) NOT NULL,
     idade DATE NOT NULL,
     genero VARCHAR(1) NOT NULL,
     email VARCHAR(100) NOT NULL,
     palavrapasse VARCHAR(100) NOT NULL,
-    hash_palavrapasse VARCHAR(10) NOT NULL,
     UNIQUE(email)
 );
