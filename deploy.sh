25767050912fd87e6cb2fa4df715406a05d7147c
bulbucus <emanuelfarinha98@gmail.com>
2021-02-20 06:18:00 +0000

    remover mapping e fix fadein no login

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i .ssh/id_ed25519 emanuelfarinha98@34.122.42.138 "cd myskynatura && git pull && sudo docker-compose down && sudo docker-compose up -d"
+ssh -i /c/Users/emanu/.ssh/id_ed25519 emanuelfarinha98@34.122.42.138 "cd myskynatura && git pull && sudo docker-compose down && sudo docker-compose up -d"
